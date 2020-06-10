f4e14c5b694a6686987e68c98f0350a4020f16a7
Seth Vargo <seth@sethvargo.com>
2020-06-10 14:33:10 -0400

    Run spellcheck, staticcheck, and vet on tests (#8)

--- tools.go
+++ tools.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     https://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,17 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-module github.com/GoogleCloudPlatform/secrets-store-csi-driver-provider-gcp
+// +build tools
 
-go 1.14
+// Package tools includes the list of tools used in the project.
+package tools
 
-require (
-	cloud.google.com/go v0.57.0
-	github.com/client9/misspell v0.3.4
-	golang.org/x/tools v0.0.0-20200501065659-ab2804fb9c9d
-	google.golang.org/api v0.22.0
-	google.golang.org/genproto v0.0.0-20200605102947-12044bf5ea91
-	google.golang.org/grpc v1.29.1
-	gopkg.in/yaml.v2 v2.3.0
-	honnef.co/go/tools v0.0.1-2020.1.3
+import (
+	_ "github.com/client9/misspell/cmd/misspell"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "honnef.co/go/tools/cmd/staticcheck"
 )
