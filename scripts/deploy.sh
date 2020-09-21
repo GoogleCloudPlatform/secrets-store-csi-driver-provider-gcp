94a0e5adadf6e525f3d6d9301670dc2cf1722e7b
Tommy Murphy <tommymurphy@google.com>
2020-09-21 13:55:00 -0400

    build: update script to use tags when doing dev deploys

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,5 +19,7 @@ set -o pipefail # Check the exit code of *all* commands in a pipeline
 set -o nounset  # Error if accessing an unbound variable
 set -x          # Print each command as it is run
 
-sed "s/\$PROJECT_ID/${PROJECT_ID}/g" deploy/provider-gcp-plugin.yaml.tmpl | kubectl apply -f -
+TAG=$(git describe --tags --exact-match 2> /dev/null || git rev-parse --short HEAD)
+
+sed "s/\$PROJECT_ID/${PROJECT_ID}/g;s/\$TAG_NAME/${TAG}/g" deploy/provider-gcp-plugin.yaml.tmpl | kubectl apply -f -
 kubectl apply -f deploy/workload-id-binding.yaml
