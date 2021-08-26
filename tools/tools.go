01532bbefc1fa2d2f7142874893d53b6105f1bb8
Tommy Murphy <tommymurphy@google.com>
2021-08-26 10:58:03 -0400

    chore: build and test against go 1.17

--- tools/tools.go
+++ tools/tools.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build tools
 // +build tools
 
 // Package tools includes the list of tools used in the project.
