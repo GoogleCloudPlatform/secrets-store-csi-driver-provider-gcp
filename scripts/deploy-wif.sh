6e565c5918636f76ccbe015b83b22066f5006373
Sergey Shcherbakov <sergey.shcherbakov@gmail.com>
2022-12-05 16:27:23 +0100

    Add Fleet Workload Identity support for pod-adc authentication mode (#190)

---
+++ scripts/deploy-wif.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit  # Exit with error on non-zero exit codes
+set -o pipefail # Check the exit code of *all* commands in a pipeline
+set -o nounset  # Error if accessing an unbound variable
+set -x          # Print each command as it is run
+
+TAG=$(git describe --tags --exact-match 2> /dev/null || git rev-parse --short HEAD)
+
+sed "s/\$PROJECT_ID/${PROJECT_ID}/g;s/\$GCP_PROVIDER_SHA/${TAG}/g" test/e2e/templates/provider-gcp-plugin-wif.yaml.tmpl | kubectl apply -f -
